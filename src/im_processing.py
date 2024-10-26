8cf41442091a7a76a17c652b964fcd37ec0ae79c
vladrosant <pedromuch1@gmail.com>
2024-10-26 17:28:19 -0300

    Change image pathing to src/images

--- src/im_processing.py
+++ src/im_processing.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/space_debris_sample.jpg', 0)
+img = cv2.imread('src/images/space_debris_sample.jpg', 0)
 
 blurred_img = cv2.GaussianBlur(img, (5, 5), 0)
 
 edges = cv2.Canny(img, 100, 200)
 
-cv2.imwrite('edges_output.jpg', edges)
+cv2.imwrite('src/images/edges_output.jpg', edges)
 
 cv2.imshow('Edges', edges)
 cv2.waitKey(0)
